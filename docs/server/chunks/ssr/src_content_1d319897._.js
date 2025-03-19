bac377819bd97071e686ad9734a130a3ca91f0e0
hieunt27 <noka.hieunt@gmail.com>
2025-03-19 14:01:36 +0700

    update

---
+++ docs/server/chunks/ssr/src_content_1d319897._.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+"[project]/src/content/about.mdx.tsx [app-rsc] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/ssr/src_f88e0f79._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/content/about.mdx.tsx [app-rsc] (ecmascript)");
+    });
+});
+}}),
+"[project]/src/content/welcome.mdx.tsx [app-rsc] (ecmascript, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "server/chunks/ssr/src_691c8d64._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/content/welcome.mdx.tsx [app-rsc] (ecmascript)");
+    });
+});
+}}),
+
+};
